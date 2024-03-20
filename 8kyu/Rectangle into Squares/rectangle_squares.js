5cc3e0a9c39f7764e75187059bb71eb27944d956
Barry Calligan <155800934+Barry1138@users.noreply.github.com>
2024-03-20 16:32:48 -0400

    Delete 8kyu/Rectangle into Squares directory

--- 8kyu/Rectangle into Squares/rectangle_squares.js
+++
@@ -1,9 +0,0 @@
-function sqInRect(lng, wdth){
-  let arr = []
-  if(lng === wdth) return null
-  while(lng > 0 && wdth > 0){
-    arr.push(lng > wdth ? wdth : lng)
-    lng > wdth ? lng -= wdth : wdth -= lng
-  }
-  return arr
-}
